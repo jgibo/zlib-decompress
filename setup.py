c7863e08cc5393f10597ad9f48a75a8375c0f018
Jordan Gibbings <jordang@familyzone.com>
2021-10-14 01:26:26 +0800

    author metadata

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from setuptools import setup, find_packages
 
-VERSION = "0.0.1"
+VERSION = "0.0.2"
 DESCRIPTION = ""
 LONG_DESCRIPTION = ""
 
 setup(
     name="zlib_decompress",
     version=VERSION,
-    author="",
-    author_email="",
+    author="Jordan Gibbings",
+    author_email="jgibbings94@gmail.com",
     description=DESCRIPTION,
     long_description=open('README.md').read(),
     url="https://github.com/jgibo/zlib-decompress",
